121dfbfc2722889eb91afc7dfca091554aeb2781
gurramoller <gurramoller@users.noreply.github.com>
2023-04-12 16:48:58 +0200

    Update weather.dart

--- healthapp/lib/backend/weather/weather.dart
+++ healthapp/lib/backend/weather/weather.dart
@@ -7,7 +7,10 @@ class apiParser{
   var ulr = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint);
   
   Null requestWeather(Float latitude, Float longitude){
-    String request = "?latitude=$latitude&longitude=$longitude";
-  
+    String request = "?latitude=$latitude&longitude=$longitude&hourly=temperature_2m,weathercode,cloudcover,precipitation,snowfall,windspeed_10m,winddirection_10m&start_date=2023-04-12&end_date=2023-04-12";
+    
   }
+
+
+
 }
