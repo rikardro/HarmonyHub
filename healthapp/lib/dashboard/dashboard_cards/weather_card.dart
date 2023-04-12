e38a76b6dc5a40f0d761ceb2239298129744bbad
andreasmagn <andreas@magn.se>
2023-04-12 16:22:56 +0200

    weather_card
    Co-Authored-By: riarkd <130552559+riarkd@users.noreply.github.com>

--- healthapp/lib/dashboard/dashboard_cards/weather_card.dart
+++ healthapp/lib/dashboard/dashboard_cards/weather_card.dart
@@ -3,12 +3,49 @@ import '../dashboard_card.dart';
 
 class WeatherCard extends StatelessWidget {
   const WeatherCard({Key? key}) : super(key: key);
-
+  
   @override
   Widget build(BuildContext context) {
-    return DashboardCard(
-      flex: 12, color: Colors.grey,
-      child: Container(), // CONTENT HERE
+    final baseTextStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 15,
+      fontFamily: 'Inter',
+      shadows: [
+        Shadow(
+          blurRadius: 10,
+          offset: const Offset(2, 2),
+          color: Colors.black.withOpacity(0.3),
+        ),
+      ],
     );
+
+    return DashboardCard(
+      flex: 12, color: const Color(0xFFFF9900),
+      child: Row(
+        children: [
+          Column(
+            children: const [
+              Image(image: AssetImage('assets/images/3d-sun-with-smile-face-cartoon-style-rendered-object-illustration-png 1.png'),
+              width: 100,),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Sunny", style: TextStyle(fontSize: 25, color: baseTextStyle.color, fontFamily: baseTextStyle.fontFamily, shadows: baseTextStyle.shadows)),
+                    Text("17 °C", style: TextStyle(fontSize: 20, color: baseTextStyle.color, fontFamily: baseTextStyle.fontFamily, shadows: baseTextStyle.shadows)),
+                    Text("3 m/s", style: TextStyle(fontSize: 15, color: baseTextStyle.color, fontFamily: baseTextStyle.fontFamily, shadows: baseTextStyle.shadows))
+                  ],
+              ),
+            ),
+            ],
+          ),
+        ],
+      ));
   }
 }
