932b74a943790f0a73f4b4c05a53db7fe67298e8
surfknasen <casper.christiansson@gmail.com>
2023-04-12 15:37:38 +0200

    Health card placeholders

--- healthapp/lib/dashboard/dashboard_cards/health_card.dart
+++ healthapp/lib/dashboard/dashboard_cards/health_card.dart
@@ -2,16 +2,58 @@ import 'package:flutter/material.dart';
 import '../dashboard_card.dart';
 
 class HealthCard extends StatelessWidget {
-  const HealthCard({Key? key, this.height = 100, this.flex = 1}) : super(key: key);
+  const HealthCard({Key? key,
+    this.height = 100,
+    this.flex = 1,
+    required this.title,
+    required this.icon,
+    required this.value,
+    this.iconColor,
+    this.topPadding = 16}) : super(key: key);
   final double? height;
   final int? flex;
+  final String title;
+  final IconData icon;
+  final Color? iconColor;
+  final String value;
+  final double topPadding;
 
   @override
   Widget build(BuildContext context) {
     return DashboardCard(
       height: height,
-      flex: flex, color: Colors.grey,
-      child: Container(), // CONTENT HERE
+      flex: flex,
+      color: Colors.white,
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Text(title, style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18, color: Colors.grey[600]),)
+          ),
+          Center(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(8, topPadding, 8, 0),
+              child: Wrap(
+                alignment: WrapAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(4, 0, 4, 0),
+                    child: Icon(icon, color: iconColor, size: 30,),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(4, 0, 4, 0),
+                    child: Text(value,
+                      style: TextStyle(
+                          fontSize: 30, fontWeight: FontWeight.w600,
+                          color: Colors.grey[700],
+                      ),),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- healthapp/lib/dashboard/dashboard_view.dart
+++ healthapp/lib/dashboard/dashboard_view.dart
@@ -1,9 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:healthapp/dashboard/dashboard_cards/air_quality_card.dart';
 import 'package:healthapp/dashboard/dashboard_cards/caffeine_card.dart';
 import 'package:healthapp/dashboard/dashboard_cards/health_card.dart';
 import 'package:healthapp/dashboard/dashboard_cards/weather_card.dart';
-import 'dashboard_card.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({Key? key}) : super(key: key);
@@ -21,13 +21,31 @@ class DashboardView extends StatelessWidget {
           ),
           Row(
             children: [
-              HealthCard(),
-              HealthCard()
+              HealthCard(
+                title: "Steps",
+                value: "3457",
+                icon: Icons.directions_walk,
+                iconColor: Colors.grey[600],
+              ),
+              const HealthCard(
+                title: "Heart",
+                value: "69",
+                icon: Icons.favorite,
+                iconColor: Colors.red
+              )
             ],
           ),
           Row(
             children: [
-              HealthCard(flex: 5, height: 120,),
+              HealthCard(
+                flex: 5,
+                height: 120,
+                title: "Flights",
+                value: "13",
+                icon: Icons.stairs,
+                iconColor: Colors.grey[600],
+                topPadding: 24,
+              ),
               CaffeineCard()
             ],
           ),
