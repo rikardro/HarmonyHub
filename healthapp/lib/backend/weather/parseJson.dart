2230ab75768b34955cba2bd732338dd1f54587ac
vistca <93658028+vistca@users.noreply.github.com>
2023-04-13 08:51:54 +0200

    changed hashmap key from string to int
    Co-Authored-By: gurramoller <74371542+gurramoller@users.noreply.github.com>

--- healthapp/lib/backend/weather/parseJson.dart
+++ healthapp/lib/backend/weather/parseJson.dart
@@ -11,31 +11,34 @@ class JsonParser{
     initWeatherTypes();
   }
 
-  Null initWeatherTypes(){
-    weatherTypeMap["clear sky"] = WeatherType.clear;
-    weatherTypeMap["mainly clear"] = WeatherType.halfCloudy;
-    weatherTypeMap["partly cloudy"] = WeatherType.halfCloudy;
-    weatherTypeMap["overcast"] = WeatherType.cloudy;
-    weatherTypeMap["fog"] = WeatherType.foggy;
-    weatherTypeMap["depositing rime fog"] = WeatherType.foggy;
-    weatherTypeMap["light drizzle"] = WeatherType.raining;
-    weatherTypeMap["moderate drizzle"] = WeatherType.raining;
-    weatherTypeMap["dense drizzle"] = WeatherType.raining;
-    weatherTypeMap["light freezing drizzle"] = WeatherType.raining;
-    weatherTypeMap["dense freezing drizzle"] = WeatherType.raining;
-    weatherTypeMap["light rain"] = WeatherType.raining;
-    weatherTypeMap["moderate rain"] = WeatherType.raining;
-    weatherTypeMap["heavy rain"] = WeatherType.raining;
-    weatherTypeMap["light freezing rain"] = WeatherType.raining;
-    weatherTypeMap["heavy freezing rain"] = WeatherType.raining;
-    weatherTypeMap["slight snow fall"] = WeatherType.snowing;
-    weatherTypeMap["moderate snow fall"] = WeatherType.snowing;
-    weatherTypeMap["heavy snow fall"] = WeatherType.snowing;
-    weatherTypeMap["snow grains"] = WeatherType.snowing;
-    weatherTypeMap["slight rain showers"] = WeatherType.raining;
-    weatherTypeMap["moderate rain showers"] = WeatherType.raining;
-    weatherTypeMap["violent rain showers"] = WeatherType.raining;
-    weatherTypeMap["slight snow showers"] = WeatherType.snowing;
-    weatherTypeMap["heavy snow showers"] = WeatherType.snowing;
+  void initWeatherTypes(){
+    weatherTypeMap[0] = WeatherType.clear; // clear sky
+    weatherTypeMap[1] = WeatherType.halfCloudy; // mainly clear
+    weatherTypeMap[2] = WeatherType.halfCloudy; // partly cloudy
+    weatherTypeMap[3] = WeatherType.cloudy; // overcast
+    weatherTypeMap[45] = WeatherType.foggy; // fog
+    weatherTypeMap[48] = WeatherType.foggy; // depositing rime fog
+    weatherTypeMap[51] = WeatherType.raining; // light drizzle
+    weatherTypeMap[53] = WeatherType.raining; // moderate drizzle
+    weatherTypeMap[55] = WeatherType.raining; // dense drizzle
+    weatherTypeMap[56] = WeatherType.raining; // light freezing drizzle
+    weatherTypeMap[57] = WeatherType.raining; // dense freezing drizzle
+    weatherTypeMap[61] = WeatherType.raining; // light rain
+    weatherTypeMap[63] = WeatherType.raining; // moderate rain
+    weatherTypeMap[65] = WeatherType.raining; // heavy rain
+    weatherTypeMap[66] = WeatherType.raining; // light freezing rain
+    weatherTypeMap[67] = WeatherType.raining; // heavy freezing rain
+    weatherTypeMap[71] = WeatherType.snowing; // slight snow fall
+    weatherTypeMap[73] = WeatherType.snowing; // moderate snow fall
+    weatherTypeMap[75] = WeatherType.snowing; // heavy snow fall
+    weatherTypeMap[77] = WeatherType.snowing; // snow grains
+    weatherTypeMap[80] = WeatherType.raining; // slight rain showers
+    weatherTypeMap[81] = WeatherType.raining; // moderate rain showers
+    weatherTypeMap[82] = WeatherType.raining; // violent rain showers
+    weatherTypeMap[85] = WeatherType.snowing; // slight snow showers
+    weatherTypeMap[86] = WeatherType.snowing; // heavy snow showers
   }
 }
+
+
+
