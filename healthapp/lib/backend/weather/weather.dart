06bb164e706dff7ceb81ff6ffd58744a6c50114b
gurramoller <gurramoller@users.noreply.github.com>
2023-04-13 09:15:10 +0200

    api stuff
    Co-Authored-By: vistca <93658028+vistca@users.noreply.github.com>

--- healthapp/lib/backend/weather/weather.dart
+++ healthapp/lib/backend/weather/weather.dart
@@ -1,16 +1,23 @@
 // Gets the information from the api in json format and parses it to a dart object
 import 'dart:ffi';
-
+import 'package:http/http.dart' as http;
 import 'package:healthapp/backend/weather/apiConstants.dart';
 
-class apiParser{
-  var ulr = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint);
+class ApiParser{
   
-  Null requestWeather(Float latitude, Float longitude){
-    String request = "?latitude=$latitude&longitude=$longitude&hourly=temperature_2m,weathercode,cloudcover,precipitation,snowfall,windspeed_10m,winddirection_10m&start_date=2023-04-12&end_date=2023-04-12";
-    
+  void requestWeather(double latitude, double longitude) async{
+    String request = "?latitude=$latitude&longitude=$longitude&hourly=temperature_2m,precipitation,snowfall,snow_depth,weathercode,cloudcover,windspeed_10m,winddirection_10m";
+    var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint + request);
+    var response = await http.get(url);
+    if(response.statusCode == 200){
+      print(response.body);
+    }
   }
 
-
-
 }
+
+  void main(){
+    ApiParser api = new ApiParser();
+    api.requestWeather(58.0, 12.0);
+  }
+
