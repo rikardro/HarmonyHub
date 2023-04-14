d36ac2dc2c6cb476eabb19e62622831ff39cf712
vistca <93658028+vistca@users.noreply.github.com>
2023-04-14 10:36:22 +0200

    exceptions handling
    Co-Authored-By: gurramoller <74371542+gurramoller@users.noreply.github.com>

--- healthapp/lib/backend/weather/weather.dart
+++ healthapp/lib/backend/weather/weather.dart
@@ -1,5 +1,6 @@
 // Gets the information from the api in json format and parses it to a dart object
 import 'dart:convert';
+import 'package:healthapp/backend/weather/customExceptions.dart';
 import 'package:healthapp/backend/weather/parseJson.dart';
 import 'package:healthapp/util/weatherInformation.dart';
 import 'package:http/http.dart' as http;
@@ -7,15 +8,16 @@ import 'package:healthapp/backend/weather/apiConstants.dart';
 
 class ApiParser{
   
-  void requestWeather(double latitude, double longitude) async{
+  Future<List<WeatherInformation>> requestWeather(double latitude, double longitude) async{
     String request = "?latitude=$latitude&longitude=$longitude&hourly=temperature_2m,precipitation,snowfall,snow_depth,weathercode,cloudcover,windspeed_10m,winddirection_10m";
     var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint + request);
     var response = await http.get(url);
     if(response.statusCode == 200){
       JsonParser jsonParser = JsonParser(response.body.toString());
       List<WeatherInformation> wi = jsonParser.jsonDataConverter();
-      print(jsonParser.hwc.temperature_2m.toString() + "\n");
-      print(wi[1].temperature.toString());
+      return wi;
+    } else {
+      throw APIException('could not load data from Meteo weather API');
     }
   }
 
