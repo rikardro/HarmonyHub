b8d7930db9a924f2cd9da7fae23ac6f4771e4ba4
riarkd <130552559+riarkd@users.noreply.github.com>
2023-04-14 11:53:24 +0200

    Weather card pictures
    Co-Authored-By: andreasmagn <93663766+andreasmagn@users.noreply.github.com>

--- healthapp/lib/dashboard/dashboard_cards/weather_card.dart
+++ healthapp/lib/dashboard/dashboard_cards/weather_card.dart
@@ -2,13 +2,82 @@ import 'package:flutter/material.dart';
 import '../dashboard_card.dart';
 
 class WeatherCard extends StatelessWidget {
-  const WeatherCard({Key? key, required this.weather, required this.degrees, required this.wind}) : super(key: key);
-  
+  const WeatherCard(
+      {Key? key,
+      required this.weather,
+      required this.degrees,
+      required this.wind})
+      : super(key: key);
+
   final String weather;
   final double degrees;
   final double wind;
 
-  
+  AssetImage weatherImage() {
+    switch (weather) {
+      case ("Sunny"):
+        {
+          return const AssetImage('assets/images/clear_day.png');
+        }
+      case ("Rainy"):
+        {
+          return const AssetImage('assets/images/raining.png');
+        }
+      case ("Cloudy"):
+        {
+          return const AssetImage('assets/images/cloudy.png');
+        }
+      case ("Foggy"):
+        {
+          return const AssetImage('assets/images/foggy.png');
+        }
+      case ("Half cloudy"):
+        {
+          return const AssetImage('assets/images/halfcloudy_day.png');
+        }
+      case ("Snowing"):
+        {
+          return const AssetImage('assets/images/snowing.png');
+        }
+      default:
+        {
+          return const AssetImage('assets/images/clear_day.png');
+        }
+    }
+  }
+
+  Color weatherColor() {
+    switch (weather) {
+      case ("Sunny"):
+        {
+          return const Color(0xFFFF9900);
+        }
+      case ("Rainy"):
+        {
+          return const Color.fromARGB(255, 71, 157, 255);
+        }
+      case ("Cloudy"):
+        {
+          return const Color.fromARGB(255, 152, 166, 182);
+        }
+      case ("Foggy"):
+        {
+          return const Color.fromARGB(255, 194, 223, 255);
+        }
+      case ("Half cloudy"):
+        {
+          return Color.fromARGB(255, 255, 216, 143);
+        }
+      case ("Snowing"):
+        {
+          return const Color.fromARGB(255, 203, 210, 255);
+        }
+      default:
+        {
+          return const Color.fromARGB(255, 114, 180, 255);
+        }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,35 +95,55 @@ class WeatherCard extends StatelessWidget {
     );
 
     return DashboardCard(
-      flex: 12, color: const Color(0xFFFF9900),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              Image(image: AssetImage('assets/images/clear_day.png'),
-              width: 100,),
-            ],
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(right: 30),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(weather, style: TextStyle(fontSize: 25, color: baseTextStyle.color, fontFamily: baseTextStyle.fontFamily, shadows: baseTextStyle.shadows, fontWeight: FontWeight.w500)),
-                    Text("$degrees °C", style: TextStyle(fontSize: 20, color: baseTextStyle.color, fontFamily: baseTextStyle.fontFamily, shadows: baseTextStyle.shadows)),
-                    Text("$wind m/s", style: TextStyle(fontSize: 16, color: baseTextStyle.color, fontFamily: baseTextStyle.fontFamily, shadows: baseTextStyle.shadows))
-                  ],
+        flex: 12,
+        color: weatherColor(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image(
+                  image: weatherImage(),
+                  width: 100,
                 ),
-              ),
-            ],
-          ),
-        ],
-      ));
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(right: 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(weather,
+                          style: TextStyle(
+                              fontSize: 25,
+                              color: baseTextStyle.color,
+                              fontFamily: baseTextStyle.fontFamily,
+                              shadows: baseTextStyle.shadows,
+                              fontWeight: FontWeight.w500)),
+                      Text("$degrees °C",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: baseTextStyle.color,
+                              fontFamily: baseTextStyle.fontFamily,
+                              shadows: baseTextStyle.shadows)),
+                      Text("$wind m/s",
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: baseTextStyle.color,
+                              fontFamily: baseTextStyle.fontFamily,
+                              shadows: baseTextStyle.shadows))
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 }
