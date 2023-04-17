64bc5089244c20b04a284e67f550931c9a784e2a
vistca <93658028+vistca@users.noreply.github.com>
2023-04-17 11:34:37 +0200

    firebase now works for the web

--- healthapp/lib/main.dart
+++ healthapp/lib/main.dart
@@ -2,9 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:healthapp/dashboard/dashboard_view.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
