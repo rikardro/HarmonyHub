71974ed9687fe0d23fa550531a9a5e466359d0eb
harryden <harry.denell@gmail.com>
2023-04-17 10:05:43 +0200

    added web support for firebase connection

--- healthapp/lib/main.dart
+++ healthapp/lib/main.dart
@@ -2,9 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:healthapp/dashboard/dashboard_view.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
