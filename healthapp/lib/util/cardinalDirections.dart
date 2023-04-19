3fbc38de25924317955f13240512e95997bf3d00
riarkd <130552559+riarkd@users.noreply.github.com>
2023-04-19 11:18:35 +0200

    Weather card wind direction

--- healthapp/lib/util/cardinalDirections.dart
+++ healthapp/lib/util/cardinalDirections.dart
@@ -8,3 +8,28 @@ enum CardinalDirection {
   southwest,
   southeast
 }
+
+extension CardinalDirectionExtension on CardinalDirection {
+  String get value {
+    switch (this) {
+      case CardinalDirection.north:
+        return 'N';
+      case CardinalDirection.south:
+        return 'S';
+      case CardinalDirection.west:
+        return 'W';
+      case CardinalDirection.east:
+        return 'E';
+      case CardinalDirection.northwest:
+        return 'NW';
+      case CardinalDirection.northeast:
+        return 'NE';
+      case CardinalDirection.southwest:
+        return 'SW';
+      case CardinalDirection.southeast:
+        return 'SE';
+      default:
+        return '';
+    }
+  }
+}
