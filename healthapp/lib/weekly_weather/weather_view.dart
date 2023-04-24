55a20da73f8ea56a78141266df1f6c86e9fd8f8d
riarkd <130552559+riarkd@users.noreply.github.com>
2023-04-24 11:54:00 +0200

    Weekly weather inf ui
    Co-Authored-By: gurramoller <74371542+gurramoller@users.noreply.github.com>

---
+++ healthapp/lib/weekly_weather/weather_view.dart
@@ -0,0 +1,62 @@
+import 'dart:developer';
+
+import 'package:geolocator/geolocator.dart';
+import 'package:healthapp/util/dialogs/error_dialog.dart';
+import 'package:healthapp/util/weatherInformation.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:healthapp/bloc/caffeine_bloc.dart';
+import 'package:healthapp/bloc/caffeine_detailed_bloc.dart';
+
+import '../backend/location/location.dart';
+import '../backend/weather/weather.dart';
+
+class WeatherDetailedView extends StatefulWidget {
+  const WeatherDetailedView({super.key});
+
+  @override
+  State<WeatherDetailedView> createState() => _WeatherDetailedViewState();
+}
+
+class _WeatherDetailedViewState extends State<WeatherDetailedView> {
+  Future<Location> fetchLocation() async {
+    return await Location.create();
+  }
+
+  Future<List<WeatherInformationWeekly>> fetchWeatherData(
+      Position position) async {
+    ApiParser apiParser = ApiParser();
+    List<WeatherInformationWeekly> wi = await apiParser.requestWeeklyWeather(
+        position.latitude, position.longitude);
+    return wi;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: fetchLocation(),
+        builder: (context, AsyncSnapshot<Location> location) {
+          if (location.hasData) {
+            return Container(
+                child:
+                    ListView(physics: BouncingScrollPhysics(), children: [
+                      Column(
+                        children: [
+                          generateWeatherCards(location.data!.position)
+                        ],
+                      )
+                    ]));
+          }
+        });
+  }
+  
+  generateWeatherCards(Position position) async {
+    List<WeatherInformationWeekly> wi = await fetchWeatherData(position);
+  }
+}
