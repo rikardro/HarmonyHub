add0dd444439a197096de8a28f84934e044ef2d8
surfknasen <casper.christiansson@gmail.com>
2023-04-24 14:32:52 +0200

    Improved points system
    Co-Authored-By: vistca <93658028+vistca@users.noreply.github.com>

--- healthapp/lib/backend/weather/recommended_days_repo.dart
+++ healthapp/lib/backend/weather/recommended_days_repo.dart
@@ -1,46 +1,77 @@
+import 'dart:math';
+
 import 'package:healthapp/backend/weather/weather.dart';
 
 import '../../util/weatherInformation.dart';
 import '../../util/weatherType.dart';
+
+class PointsData{
+  final double windPoints;
+  final double temperaturePoints;
+  final double snowPoints;
+  final double weatherTypePoints;
+  final double precipitationPoints;
+
+  double getTotalPoints(){
+    return windPoints + temperaturePoints + snowPoints + weatherTypePoints + precipitationPoints;
+  }
+
+  void printPoints(){
+    print("POINTS!:::");
+    print("Wind: $windPoints");
+    print("Temperature: $temperaturePoints");
+    print("Snow: $snowPoints");
+    print("Weather: $weatherTypePoints");
+    print("Precipitation: $precipitationPoints");
+    print("--------------------");
+  }
+
+  PointsData({required this.windPoints, required this.temperaturePoints, required this.snowPoints, required this.weatherTypePoints, required this.precipitationPoints});
+}
 
 class RecommendedDaysRepo {
   final ApiParser apiClient;
 
   RecommendedDaysRepo({required this.apiClient}) : assert(apiClient != null);
 
-  double calculatePoints(WeatherInformation weatherData, {int idealTemperature = 18}) {
-    double points = 0;
+  double proximityWeight(double diff, double k, double limit){
+    double funcVal = (diff * k).abs() - limit * k;
+    return max(funcVal, 0);
+  }
+
+  PointsData calculatePoints(WeatherInformation weatherData, {int idealTemperature = 18}) {
 
     // Wind
-    points -= (weatherData.windspeed / 3).floor();
+    double windPoints = -(weatherData.windspeed / 5);
 
     // Temperature
-    points -= (weatherData.temperature - idealTemperature).abs().round();
+    //points -= pow((weatherData.temperature - idealTemperature).abs().round()/2, 2);
+    double tempPoints = -(weatherData.temperature - idealTemperature).abs() * 3;
+    //points += proximityWeight((weatherData.temperature - idealTemperature).abs(), 1, 0.5);
 
     // Snow depth
+    double snowPoints = 0;
     if (weatherData.snow_depth > 0) {
-      points -= 10;
+      snowPoints = -10;
     }
 
     // Weather type
-    Map<WeatherType, int> weatherTypePoints = {
-      WeatherType.clear: 5,
-      WeatherType.cloudy: 0,
-      WeatherType.foggy: -1,
-      WeatherType.halfCloudy: 3,
-      WeatherType.raining: -3,
-      WeatherType.snowing: 0
+    Map<WeatherType, double> weatherTypePoints = {
+      WeatherType.clear: 10,
+      WeatherType.cloudy: 3,
+      WeatherType.foggy: -3,
+      WeatherType.halfCloudy: 6,
+      WeatherType.raining: -6,
+      WeatherType.snowing: -3
     };
-    points += weatherTypePoints[weatherData.weather] ?? 0;
+    double weatherPoints = weatherTypePoints[weatherData.weather] ?? 0;
 
     // Precipitation
-    points -= weatherData.precipitation.round();
+    double precipitationPoints = -(weatherData.precipitation * 30);
 
-    // Since humidity is not available in the provided class, it has been omitted from the calculation.
+    // TODO add humidity and air quality
 
-    // Air quality (AQI) is also not available in the provided class, so it has been omitted from the calculation.
-
-    return points;
+    return PointsData(windPoints: windPoints, temperaturePoints: tempPoints, snowPoints: snowPoints, weatherTypePoints: weatherPoints, precipitationPoints: precipitationPoints);
   }
 
   // Cluster times into intervals
@@ -57,46 +88,43 @@ class RecommendedDaysRepo {
         if(currentCluster.isEmpty){
           currentCluster.add(recommendedTimes[i]);
         }
-        else if((currentCluster.last.points - recommendedTimes[i].points).abs() <= 2){
+        else if((currentCluster.last.points.getTotalPoints() - recommendedTimes[i].points.getTotalPoints()).abs() <= 2.5){
           currentCluster.add(recommendedTimes[i]);
         } else if(currentCluster.isNotEmpty){
           clusters.add(currentCluster.toList());
           currentCluster.clear();
         }
-      } else {
-        print('The time is not between 05:00 and 22:00');
       }
     }
     
     return clusters;
   }
 
-  Future<List<RecommendedIntervals>> getRecommended() async{
-    final apiClient = ApiParser();
-
+  Future<List<RecommendedIntervals>> getRecommended(int amount) async{
     List<WeatherInformation> weatherList = await apiClient.requestWeather(57.7085865,11.9684324);
     List<RecommendedTime> recommended = [];
     for (WeatherInformation weather in weatherList) {
-      double points = await calculatePoints(weather);
+      PointsData points = calculatePoints(weather);
       recommended.add(RecommendedTime(weather, points));
     }
 
     final List<List<RecommendedTime>> clusters = clusterTimes(recommended);
 
-    // Sort clusters by the average points in descending order
-    clusters.sort((a, b) => b.fold(0, (sum, e) => sum + e.points.truncate()) ~/ b.length 
-                  - a.fold(0, (sum, e) => sum + e.points.truncate()) ~/ a.length);
+    // Sort clusters by the average points
+    clusters.sort((a, b) => (b.fold(0.0, (sum, e) => sum + e.points.getTotalPoints()) / b.length
+                  - a.fold(0.0, (sum, e) => sum + e.points.getTotalPoints()) / a.length).toInt());
 
-    List<List<RecommendedTime>> topClusters = clusters.take(5).toList();
+    List<List<RecommendedTime>> topClusters = clusters.take(amount).toList();
     List<RecommendedIntervals> recommendedIntervals = [];
     for (List<RecommendedTime> cluster in topClusters) {
       recommendedIntervals.add(RecommendedIntervals(
         DateTime.parse(cluster.first.weather.time),
         DateTime.parse(cluster.last.weather.time),
         // Get average temperature
-        cluster.fold(0, (sum, e) => sum + e.weather.temperature.truncate()) / cluster.length,
-        cluster.fold(0, (sum, e) => sum + e.weather.windspeed.truncate()) / cluster.length,
-        cluster.fold(0, (sum, e) => sum + e.weather.precipitation.truncate()) / cluster.length,
+        cluster.fold(0.0, (sum, e) => sum + e.weather.temperature) / cluster.length,
+        cluster.fold(0.0, (sum, e) => sum + e.weather.windspeed) / cluster.length,
+        cluster.fold(0.0, (sum, e) => sum + e.weather.precipitation) / cluster.length,
+        cluster.map((x) => x.points).toList(),
         cluster.map((x) => x.weather.weather).toSet()
       ));
     }
@@ -104,11 +132,32 @@ class RecommendedDaysRepo {
     return recommendedIntervals;
   }
 
+  void printIntervals(List<RecommendedIntervals> intervals){
+    // print out the recommended intervals
+    for (RecommendedIntervals interval in intervals) {
+      print('Start time: ${interval.startTime}');
+      print('End time: ${interval.endTime}');
+      print('Temperature: ${interval.temperature}');
+      print('Windspeed: ${interval.windspeed}');
+      print('Precipitation: ${interval.precipitation}');
+      print('Weather types: ${interval.weatherTypes}');
+
+      print('Total points: ${interval.points.fold(0.0, (sum, e) => sum + e.getTotalPoints())}');
+      
+      // print all points in interval
+      for (PointsData points in interval.points) {
+        points.printPoints();
+      }
+
+      print("=====================================");
+    }
+  }
+
 }
 
 class RecommendedTime{
   final WeatherInformation weather;
-  final double points;
+  final PointsData points;
 
   RecommendedTime(this.weather, this.points);
 }
@@ -120,7 +169,8 @@ class RecommendedIntervals{
   final double temperature;
   final double windspeed;
   final double precipitation;
+  final List<PointsData> points;
   final Set<WeatherType> weatherTypes;
 
-  RecommendedIntervals(this.startTime, this.endTime, this.temperature, this.windspeed, this.precipitation, this.weatherTypes);
+  RecommendedIntervals(this.startTime, this.endTime, this.temperature, this.windspeed, this.precipitation, this.points, this.weatherTypes);
 }
