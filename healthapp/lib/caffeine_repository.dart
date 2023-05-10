3b1d4e8c337b38b713efa055021a723e73982184
vistca <93658028+vistca@users.noreply.github.com>
2023-05-10 10:33:06 +0200

    reads data from yesterday
    Co-Authored-By: Andreas Magnusson <93663766+andreasmagn@users.noreply.github.com>

--- healthapp/lib/caffeine_repository.dart
+++ healthapp/lib/caffeine_repository.dart
@@ -17,8 +17,9 @@ class CaffeineRepository {
   /// Returns the current caffeine level
   Future<Caffeine> fetchCurrentCaffeine() async {
     final today = DateTime.now();
-    final startOfToday = Timestamp.fromDate(
-        DateTime(today.year, today.month, today.day, 0, 0, 0, 0, 0));
+    final yesterday = DateTime.now().subtract(const Duration(days: 1));
+    final startOfYesterday = Timestamp.fromDate(
+        DateTime(yesterday.year, yesterday.month, yesterday.day, 0, 0, 0, 0, 0));
 
     final endOfToday = Timestamp.fromDate(
         DateTime(today.year, today.month, today.day, 23, 59, 59, 999, 999));
@@ -33,7 +34,7 @@ class CaffeineRepository {
     /// Get all the caffeine consumed today
     final QuerySnapshot querySnapshot = await instance
         .where('userId', isEqualTo: currentUserId)
-        .where('timeConsumed', isGreaterThanOrEqualTo: startOfToday)
+        .where('timeConsumed', isGreaterThanOrEqualTo: startOfYesterday)
         .where('timeConsumed', isLessThanOrEqualTo: endOfToday)
         .get();
 
