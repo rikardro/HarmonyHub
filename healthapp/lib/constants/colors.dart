b32eff11c4ff53a5546aced462a590a6ed591f33
harryden <harry.denell@gmail.com>
2023-05-15 11:09:03 +0200

    added constant file for colors

---
+++ healthapp/lib/constants/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const Color sunnyDayColor = Color(0xFFFF9900);
+const Color cloudyDayColor = Color.fromARGB(255, 152, 166, 182);
+const Color foggyDayColor = Color.fromARGB(255, 194, 223, 255);
+const Color snowyDayColor = Color.fromARGB(255, 203, 210, 255);
+const Color rainyDayColor = Color.fromARGB(255, 115, 22, 255);
+const Color halfCloudyDayColor = Color.fromARGB(255, 255, 216, 143);
+
+const Color nightColor = Color.fromARGB(255, 115, 22, 255);
+
+const Color airQualityCardColor = Color(0xFF7EC9FF);
+const Color quoteCardColor = Color.fromARGB(255, 80, 179, 56);
+const Color runTrackerCardColor = Color.fromARGB(255, 49, 204, 207);
+const Color musicCardColor = Color(0xFFFA7DCA);
+
+const Color green = Color(0xFF05FF00);
+const Color yellow = Color(0xFFFFF500);
+const Color lightBlue = Color(0xFF00FFFF);
+const Color red = Color(0xFFFF0000);
+const Color orange = Color.fromARGB(255, 255, 184, 78);
--- healthapp/lib/util/weatherVisuals.dart
+++ healthapp/lib/util/weatherVisuals.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -9,8 +7,11 @@ import 'package:healthapp/backend/weather/weather.dart';
 import 'package:healthapp/util/weatherType.dart';
 import 'package:healthapp/util/weatherVisualizationInfo.dart';
 
-class WeatherVisuals{
-  static Future<WeatherVisualizationInfo> getWeatherVisuals(String weather) async{
+import '../constants/colors.dart';
+
+class WeatherVisuals {
+  static Future<WeatherVisualizationInfo> getWeatherVisuals(
+      String weather) async {
     Location loc = await Location.getInstance();
     SunUp sunInfo = await ApiParser().getSunUp(loc.latitude, loc.longitude);
     bool isDay = sunInfo.currentSunIsUp();
@@ -27,104 +28,88 @@ class WeatherVisuals{
   }
 
   static WeatherVisualizationInfo _getData(String weather, bool isDay) {
-  switch (weather) {
-    case ("clear"):
-      {
-        if (isDay) {
-          return WeatherVisualizationInfo(
-          "Sunny", 
-          AssetImage('assets/images/clear_day.png'), 
-          Color(0xFFFF9900)
-          );
-        } else {
-          return WeatherVisualizationInfo(
-          "Sunny", 
-          AssetImage('assets/images/clear_night.png'), 
-          Color.fromARGB(255, 115, 22, 255)
-          );
+    switch (weather) {
+      case ("clear"):
+        {
+          if (isDay) {
+            return WeatherVisualizationInfo("Sunny",
+                AssetImage('assets/images/clear_day.png'), sunnyDayColor);
+          } else {
+            return WeatherVisualizationInfo("Sunny",
+                AssetImage('assets/images/clear_night.png'), nightColor);
+          }
         }
-      }
-    case ("cloudy"):
-      {
-        if (isDay) {
-          return WeatherVisualizationInfo(
-          "Cloudy", 
-          AssetImage('assets/images/cloudy.png'), 
-          Color.fromARGB(255, 152, 166, 182)
-          );
-        } else {
-          return WeatherVisualizationInfo(
-          "Cloudy", 
-          AssetImage('assets/images/cloudy.png'), 
-          Color.fromARGB(255, 115, 22, 255)
-          );
+      case ("cloudy"):
+        {
+          if (isDay) {
+            return WeatherVisualizationInfo(
+              "Cloudy",
+              AssetImage('assets/images/cloudy.png'),
+              cloudyDayColor,
+            );
+          } else {
+            return WeatherVisualizationInfo(
+              "Cloudy",
+              AssetImage('assets/images/cloudy.png'),
+              nightColor,
+            );
+          }
         }
-      } 
-    case ("foggy"):
-      {
-        if (isDay) {
-          return WeatherVisualizationInfo(
-          "Foggy", 
-          AssetImage('assets/images/foggy.png'), 
-          Color.fromARGB(255, 194, 223, 255)
-          );
-        } else {
-          return WeatherVisualizationInfo(
-          "Foggy", 
-          AssetImage('assets/images/foggy.png'), 
-          Color.fromARGB(255, 115, 22, 255)
-          );
+      case ("foggy"):
+        {
+          if (isDay) {
+            return WeatherVisualizationInfo(
+              "Foggy",
+              AssetImage('assets/images/foggy.png'),
+              foggyDayColor,
+            );
+          } else {
+            return WeatherVisualizationInfo(
+                "Foggy", AssetImage('assets/images/foggy.png'), nightColor);
+          }
         }
-      }    
-    case ("snowing"):
-      {
-        if (isDay) {
-          return WeatherVisualizationInfo(
-          "Snowing", 
-          AssetImage('assets/images/snowing.png'), 
-          Color.fromARGB(255, 203, 210, 255)
-          );
-        } else {
-          return WeatherVisualizationInfo(
-          "Snowing", 
-          AssetImage('assets/images/snowing.png'), 
-          Color.fromARGB(255, 115, 22, 255)
-          );
+      case ("snowing"):
+        {
+          if (isDay) {
+            return WeatherVisualizationInfo(
+              "Snowing",
+              AssetImage('assets/images/snowing.png'),
+              snowyDayColor,
+            );
+          } else {
+            return WeatherVisualizationInfo(
+                "Snowing", AssetImage('assets/images/snowing.png'), nightColor);
+          }
         }
-      }
-    case ("raining"):
-      {
-        if (isDay) {
-          return WeatherVisualizationInfo(
-          "Raining", 
-          AssetImage('assets/images/raining.png'), 
-          Color.fromARGB(255, 137, 192, 255)
-          );
-        } else {
-          return WeatherVisualizationInfo(
-          "Raining", 
-          AssetImage('assets/images/raining.png'), 
-          Color.fromARGB(255, 115, 22, 255)
-          );
+      case ("raining"):
+        {
+          if (isDay) {
+            return WeatherVisualizationInfo(
+              "Raining",
+              AssetImage('assets/images/raining.png'),
+              rainyDayColor,
+            );
+          } else {
+            return WeatherVisualizationInfo(
+              "Raining",
+              AssetImage('assets/images/raining.png'),
+              nightColor,
+            );
+          }
         }
-      }
-    default:
-      {
-        if (isDay) {
-          return WeatherVisualizationInfo(
-          "Half cloudy", 
-          AssetImage('assets/images/halfcloudy_day.png'), 
-          Color.fromARGB(255, 255, 216, 143)
-          );
-        } else {
-          return WeatherVisualizationInfo(
-          "Half cloudy", 
-          AssetImage('assets/images/halfcloudy_night.png'), 
-          Color.fromARGB(255, 115, 22, 255)
-          );
+      default:
+        {
+          if (isDay) {
+            return WeatherVisualizationInfo(
+              "Half cloudy",
+              AssetImage('assets/images/halfcloudy_day.png'),
+              halfCloudyDayColor,
+            );
+          } else {
+            return WeatherVisualizationInfo("Half cloudy",
+                AssetImage('assets/images/halfcloudy_night.png'), nightColor);
+          }
         }
-      }
-    }  
+    }
   }
-
 }
